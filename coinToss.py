efcc76d460a5ed7a15f0997f8dca4ddb25d01b23
acastro84 <61216893+acastro84@users.noreply.github.com>
2021-05-18 11:19:47 -0500

    Create coinToss.py

---
+++ coinToss.py
@@ -0,0 +1,35 @@
+# used to help learn about probability.  
+# Returns a count  and the state of each flip. 
+
+
+import random
+class Coin:
+
+
+
+    def __init__(self):
+        self.sideup = 'Heads'
+
+
+
+    def toss(self):
+        if random.randint(0, 1) == 0:
+            self.sideup = 'Heads'
+        else:
+            self.sideup = 'Tails'
+    def get_sideup(self):
+        return self.sideup
+    
+
+def main():
+    my_coin = Coin()
+    print('This side is up', my_coin.get_sideup())
+
+
+    print('I am tossing the coin.')
+    for x in range(4):
+        my_coin.toss()
+
+        print('This side is up:',my_coin.get_sideup())
+
+main()
